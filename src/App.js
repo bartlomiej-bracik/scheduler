55823a9a2c025aa5d7e2031847ffc9b4069e030e
Bartomiej Bracik <bartlomiej.bracik@gmail.com>
2024-09-01 11:15:37 +0200

    Scheduler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Paper>
+    <Scheduler data={schedulerData}>
+      <ViewState currentDate={currentDate} />
+      <DayView startDayHour={9} endDayHour={14} />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+
+
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const appointments = [
+    {
+      id: 0,
+      title: 'Spotkanie z klientem',
+      startDate: '2024-09-01T09:00:00',
+      endDate: '2024-09-01T10:00:00',
+    },
+    {
+      id: 1,
+      title: 'Spotkanie z zespołem',
+      startDate: '2024-09-02T11:00:00',
+      endDate: '2024-09-02T12:00:00',
+    },
+  ];
